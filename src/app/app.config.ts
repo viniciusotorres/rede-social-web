ce7953ffe1cab726e25641036486dd9024f290f6
viniciusotorres <viniciustorres29@gmail.com>
2024-11-11 20:58:50 -0300

    feat: Foi desenvolvido nova interface do perfil.
    - Foi integrado os ultimos post dentro do perfil do usuário logado.
    
    - Foi iniciado o deesnvolimento de notificar o usuario a respeito das requisições.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,41 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { provideRouter, withComponentInputBinding, withHashLocation } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
+import { authInterceptor } from './modules/core/interceptors/auth.interceptor';
+
+
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch()), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding(), withHashLocation()),
+    provideAnimations(),
+    provideEnvironmentNgxMask(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline', color: 'primary' } as MatFormFieldDefaultOptions },
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      closeButton: true,
+      progressBar: true,
+      progressAnimation: 'increasing',
+      newestOnTop: true,
+      enableHtml: true,
+      tapToDismiss: true,
+    }),
+    importProvidersFrom(NgxSpinnerModule.forRoot({
+      type: 'timer'
+    })),
+  ],
 };
